bdcd5458f16f155e85d86465a8ec8a2ab6c3cd36
student <stu@ravtech.local>
2019-10-23 13:57:26 +0300

    OOP basics.

--- oop.py
+++ oop.py
@@ -1,9 +1,13 @@
-class Person:
+class PersonBase:
+    def __init__(self, surname='Doe'):
+        self.surname = surname
+
+
+class Person(PersonBase):                     # inheritance
     ('The class represents person data.')
 
-    surname = 'Doe'
-
     def __init__(self, name='Nick', age=20):  # constructor
+        PersonBase.__init__(self, surname='Doe')
         self.name = name                      # no need to create fields separately
         self.age = age
 
@@ -15,8 +19,34 @@ class Person:
     def get_person_data_static(self):
         return 'Hello! My name is %s %s. And I\'m %s years old.' % (self.name, self.surname, self.age)
 
+    def __cmp__(self, other):
+        if self.age > other.age:
+            return self.name
+        elif self.age < other.age:
+            return other.name
+        else:
+            return False
+
 
 john = Person('John')
 print('Dynamic: %s' % john.get_person_data())
 print('Static: %s' % Person.get_person_data_static(john))
 print(john.__doc__)
+
+jeff = Person('Jeff', 35)
+
+result = Person.__cmp__(john, jeff)
+if result:
+    print('%s is older.' % result)
+else:
+    print('%s and %s are of the same age' % (john.name, jeff.name))
+
+# adding a method to the class
+
+
+def sing(self):
+    return 'La-la-la!'
+
+
+Person.sing = sing
+print(john.sing())
